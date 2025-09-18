330b8db03e8e6b7d747c6645c8cdae0540c4e7a1
Jaemin Oh <ojm6135@naver.com>
2025-09-18 23:07:16 +0900

    build: 프로젝트 생성

---
+++ src/main/java/com/wagglex2/waggle/WaggleApplication.java
@@ -0,0 +1,13 @@
+package com.wagglex2.waggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaggleApplication.class, args);
+	}
+
+}
