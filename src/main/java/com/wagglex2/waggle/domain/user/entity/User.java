fc7fb2cbae660d3e24b1e89e7cce149bd52641d8
ming920 <you@example.com>
2025-09-21 12:43:57 +0900

    feat: User Domain 일부 구현

--- src/main/java/com/wagglex2/waggle/domain/user/entity/User.java
+++ src/main/java/com/wagglex2/waggle/domain/user/entity/User.java
@@ -1,4 +1,60 @@
 package com.wagglex2.waggle.domain.user.entity;
 
+import com.wagglex2.waggle.domain.user.entity.type.UserRoleType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String shortIntro;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRoleType role;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Builder
+    private User(String username, String password, String email, String nickname, String shortIntro, UserRoleType role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.nickname = nickname;
+        this.shortIntro = shortIntro;
+        this.role = role;
+    }
 }
--- src/main/java/com/wagglex2/waggle/domain/user/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/wagglex2/waggle/domain/user/service/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,17 @@
 package com.wagglex2.waggle.domain.user.service.serviceImpl;
 
+import com.wagglex2.waggle.domain.user.repository.UserRepository;
 import com.wagglex2.waggle.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+
 }
