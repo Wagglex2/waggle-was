a3935cef0176cef6f49d0576471a38afe300e2e7
ming920 <rkfsky920@gmail.com>
2025-09-26 01:41:20 +0900

    feat: User Entity 구현

---
+++ src/main/java/com/wagglex2/waggle/domain/user/entity/type/University.java
@@ -0,0 +1,39 @@
+package com.wagglex2.waggle.domain.user.entity.type;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.wagglex2.waggle.common.error.ErrorCode;
+import com.wagglex2.waggle.common.exception.BusinessException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum University {
+    YOUNGNAM_UNIV("영남대", "ynu.ac.kr"),
+    KYUNGBUK_UNIV("경북대", "knu.ac.kr"),
+    KUMOH_UNIV("금오공대", "kumoh.ac.kr"),
+    GYEONGSANG_NATIONAL_UNIV("경상국립대", "gnu.ac.kr"),
+    POSTECH("포항공대", "postech.ac.kr"),
+    DAEGU_UNIV("대구대", "deagu.ac.kr"),
+    KEIMYUNG_UNIV("계명대", "kmu.ac.kr");
+
+    private final String desc;
+    private final String domain;
+
+    public  String getName() {
+        return this.name();
+    }
+
+    public static University fromEmail(String email) {
+        String domainPart = email.substring(email.indexOf("@") + 1);
+
+        for (University university : values()) {
+            if (university.getDomain().equalsIgnoreCase(domainPart)) {
+                return university;
+            }
+        }
+
+        throw new BusinessException(ErrorCode.UNSUPPORTED_UNIVERSITY_DOMAIN);
+    }
+}
