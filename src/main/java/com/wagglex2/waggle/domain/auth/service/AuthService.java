2e5fa80ee4e51c2bbcf900b1632fbacd4fe15bfb
ming920 <rkfsky920@gmail.com>
2025-09-29 16:50:48 +0900

    refactor: SecurityConfig import 수정

--- src/main/java/com/wagglex2/waggle/domain/auth/service/AuthService.java
+++ src/main/java/com/wagglex2/waggle/domain/auth/service/AuthService.java
@@ -2,8 +2,6 @@ package com.wagglex2.waggle.domain.auth.service;
 
 import com.wagglex2.waggle.domain.auth.dto.request.SignInRequestDto;
 import com.wagglex2.waggle.domain.auth.dto.response.TokenPair;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
     void sendAuthCode(String toEmail);
