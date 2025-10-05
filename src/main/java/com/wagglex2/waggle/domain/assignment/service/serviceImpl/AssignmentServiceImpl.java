eed1e41fcef4c411bbb1511909520f037538df65
d4eh0 <eogud3332@naver.com>
2025-10-05 11:36:05 +0900

    feat: 과제 공고 등록 기능 구현 - AssignmentResponseDto 추가 - Controller, Service, Repository 메서드 구현 - Repository 테스트 코드 반영

--- src/main/java/com/wagglex2/waggle/domain/assignment/service/serviceImpl/AssignmentServiceImpl.java
+++ src/main/java/com/wagglex2/waggle/domain/assignment/service/serviceImpl/AssignmentServiceImpl.java
@@ -1,6 +1,9 @@
 package com.wagglex2.waggle.domain.assignment.service.serviceImpl;
 
+import com.wagglex2.waggle.common.error.ErrorCode;
+import com.wagglex2.waggle.common.exception.BusinessException;
 import com.wagglex2.waggle.domain.assignment.dto.request.AssignmentCreationRequestDto;
+import com.wagglex2.waggle.domain.assignment.dto.response.AssignmentResponseDto;
 import com.wagglex2.waggle.domain.assignment.entity.Assignment;
 import com.wagglex2.waggle.domain.assignment.repository.AssignmentRepository;
 import com.wagglex2.waggle.domain.assignment.service.AssignmentService;
@@ -25,5 +28,17 @@ public class AssignmentServiceImpl implements AssignmentService {
         Assignment newAssignment = AssignmentCreationRequestDto.toEntity(user, requestDto);
 
         return assignmentRepository.save(newAssignment).getId();
+    }
+
+    @Transactional
+    @Override
+    public AssignmentResponseDto getAssignment(Long assignmentId) {
+        int updated = assignmentRepository.increaseViewCount(assignmentId);
+        if (updated == 0) {
+            throw new BusinessException(ErrorCode.ASSIGNMENT_NOT_FOUND);
+        }
+
+        return assignmentRepository.findById(assignmentId)
+                .map(AssignmentResponseDto::fromEntity).get();
     }
 }
