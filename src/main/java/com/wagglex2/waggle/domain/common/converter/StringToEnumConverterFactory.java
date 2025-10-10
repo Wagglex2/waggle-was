c9f3baedac3b9ae5287e727b7f184dfd382aff25
Jaemin Oh <ojm6135@naver.com>
2025-10-11 07:45:09 +0900

    feat: `MethodArgumentTypeMismatchException` 글로벌 예외 처리 추가, StringToEnumConverterFactory 주석 추가

--- src/main/java/com/wagglex2/waggle/domain/common/converter/StringToEnumConverterFactory.java
+++ src/main/java/com/wagglex2/waggle/domain/common/converter/StringToEnumConverterFactory.java
@@ -4,7 +4,19 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 
 /**
- * 모든 enum에 대해 String -> Enum 변환
+ * 모든 Enum 타입에 대해 {@link String} -> {@link Enum} 변환 지원
+ *
+ * <p>특징:</p>
+ * <ul>
+ *     <li>Controller에서 쿼리 파라미터로 전달된 문자열을 Enum으로 자동 변환</li>
+ *     <li>소문자, 공백, 하이픈(-) 등을 Enum 형식에 맞게 처리</li>
+ *     <li>예: "my-value" -> "MY_VALUE"로 변환 후 Enum.valueOf 적용</li>
+ * </ul>
+ *
+ * <p>사용 목적:</p>
+ * <ul>
+ *     <li>Controller에서 소문자 형태의 Enum 값을 바인딩할 때 편리하게 사용</li>
+ * </ul>
  */
 public final class StringToEnumConverterFactory implements ConverterFactory<String, Enum> {
 
