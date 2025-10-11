2252051e82c106a504f1b351454908f42e5584c1
d4eh0 <eogud3332@naver.com>
2025-10-11 17:15:32 +0900

    feat: 스터디 공고 상세 조회 API 구현

--- src/main/java/com/wagglex2/waggle/domain/study/service/serviceImpl/StudyServiceImpl.java
+++ src/main/java/com/wagglex2/waggle/domain/study/service/serviceImpl/StudyServiceImpl.java
@@ -1,6 +1,9 @@
 package com.wagglex2.waggle.domain.study.service.serviceImpl;
 
+import com.wagglex2.waggle.common.error.ErrorCode;
+import com.wagglex2.waggle.common.exception.BusinessException;
 import com.wagglex2.waggle.domain.study.dto.request.StudyCreationRequestDto;
+import com.wagglex2.waggle.domain.study.dto.response.StudyResponseDto;
 import com.wagglex2.waggle.domain.study.entity.Study;
 import com.wagglex2.waggle.domain.study.repository.StudyRepository;
 import com.wagglex2.waggle.domain.study.service.StudyService;
@@ -24,5 +27,19 @@ public class StudyServiceImpl implements StudyService {
         Study newStudy = StudyCreationRequestDto.toEntity(user, requestDto);
 
         return studyRepository.save(newStudy).getId();
+    }
+
+    @Transactional
+    @Override
+    public StudyResponseDto getStudy(Long studyId) {
+        int updated = studyRepository.increaseViewCount(studyId);
+        if (updated == 0) {
+            throw new BusinessException(ErrorCode.STUDY_NOT_FOUND);
+        }
+
+        Study study = studyRepository.findById(studyId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.STUDY_NOT_FOUND));
+
+        return StudyResponseDto.fromEntity(study);
     }
 }
